fb45f08c5d15c19880bed3b660b9567aa9a13201
terrynyam <terrynyamz93@gmail.com>
2025-01-14 21:53:46 +0200

    initial commit

---
+++ src/test/java/com/tenicsolutions/pft_api_gateway/PftApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenicsolutions.pft_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PftApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
